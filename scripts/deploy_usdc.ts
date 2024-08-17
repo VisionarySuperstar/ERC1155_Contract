0d53050eed1bb344e53f2de667846e445ba43928
Superstar-lucas <lucaslee971127@gmail.com>
2024-08-18 03:50:16 +0800

    added some

--- scripts/deploy_usdc.ts
+++ scripts/deploy_usdc.ts
@@ -6,13 +6,13 @@ async function main() {
     // Retrieve the first signer, typically the default account in Hardhat, to use as the deployer.
     const [deployer] = await ethers.getSigners()
     console.log('Contract is deploying...')
-    const instanceNFT = await ethers.getContractFactory('CustomNFT');
-    const NFTContract = await instanceNFT.deploy(); // base
+    const instanceUSDC = await ethers.getContractFactory('USDCToken');
+    const USDCContract = await instanceUSDC.deploy(); // base
     // Waiting for the contract deployment to be confirmed on the blockchain.
-    await NFTContract.waitForDeployment()
+    await USDCContract.waitForDeployment()
 
     // Logging the address of the deployed My404 contract.
-    console.log(`NFT contract is deployed. Token address: ${NFTContract.target}`)
+    console.log(`USDC contract is deployed. Token address: ${USDCContract.target}`)
 }
 
 // This pattern allows the use of async/await throughout and ensures that errors are caught and handled properly.
